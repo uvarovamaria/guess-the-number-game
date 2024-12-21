fd23040135e611df7ee6bf1912979be8ef90a41b
Maria <uvarovamaria205@gmail.com>
2024-12-21 14:25:54 +0300

    Add difficulty levels

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-using System;
+Console.WriteLine("Выберите уровень сложности:");
+Console.WriteLine("1 - Легкий (1-50)");
+Console.WriteLine("2 - Средний (1-100)");
+Console.WriteLine("3 - Сложный (1-200)");
 
-class Program
+int difficulty = int.Parse(Console.ReadLine());
+int maxNumber = difficulty switch
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
-        // Основная игровая логика будет добавлена позже
-    }
-}
+    1 => 50,
+    2 => 100,
+    3 => 200,
+    _ => 100
+};
+
+int targetNumber = random.Next(1, maxNumber + 1);
