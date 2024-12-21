aaf6d33386b5b63db64daec79499650998ecca69
Maria <uvarovamaria205@gmail.com>
2024-12-21 14:24:34 +0300

    Add game logic

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
-        // Основная игровая логика будет добавлена позже
+        Random random = new Random();
+        int targetNumber = random.Next(1, 101);
+        int attempts = 0;
+        int guess = 0;
+
+        while (guess != targetNumber)
+        {
+            Console.Write("Введите число (от 1 до 100): ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < targetNumber)
+                Console.WriteLine("Загаданное число больше.");
+            else if (guess > targetNumber)
+                Console.WriteLine("Загаданное число меньше.");
+            else
+                Console.WriteLine($"Поздравляем! Вы угадали число за {attempts} попыток.");
+        }
     }
 }
