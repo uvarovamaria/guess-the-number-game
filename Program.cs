c70ddc5626d5fc9b157be794e14fb5595812e31c
Maria <uvarovamaria205@gmail.com>
2024-12-21 14:18:15 +0300

    Initial commit with project structure

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
+        // Основная игровая логика будет добавлена позже
+    }
+}
